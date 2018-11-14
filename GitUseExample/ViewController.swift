a1f59b47af606542b739974124a1dc152fbf96b2
Singam Setty, Kireeti <201418@s020rmc148-18.local>
2018-11-14 10:01:06 -0600

    hi

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Singam Setty, Kireeti on 11/14/18.
+//  Copyright © 2018 Singam Setty, Kireeti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        print("This is a Git tutorial")
+    }
+}
+
